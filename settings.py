b6072faf5959822e857d9bbba97814e7beabb5cf
NikFMatten <nfmatten@gmail.com>
2023-01-21 13:59:50 -0700

    feat: created ship speed with the value of 1.5 pixels

--- settings.py
+++ settings.py
@@ -10,3 +10,6 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230) # Default bg color
         # self.bg_color = (35,35,35) # 'Space' color
+
+        # Ship Settings
+        self.ship_speed = 1.5
