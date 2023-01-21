da3cd28b2dde73dc6569568ef803dd2112a7492a
NikFMatten <nfmatten@gmail.com>
2023-01-21 12:30:38 -0700

    feat: instantiated Settings and set screen size and background colors with Settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     """
@@ -9,8 +10,13 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game and create game resources"""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        # Set background color
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -20,6 +26,9 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
+            # Redraw the screen during each pass through the loop
+            self.screen.fill(self.settings.bg_color)
+
             # Make the most recently drawn screen visible        
             pygame.display.flip()
 
