502e67e58992e9f9538dfe3358cc20f1af1a7535
NikFMatten <nfmatten@gmail.com>
2023-01-22 11:00:18 -0700

    feat: created settings for the bullet + changed the bg color to a 'Space' color

--- settings.py
+++ settings.py
@@ -8,8 +8,14 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230) # Default bg color
-        # self.bg_color = (35,35,35) # 'Space' color
+        # self.bg_color = (230, 230, 230) # Default bg color
+        self.bg_color = (35,35,35) # 'Space' color
 
         # Ship Settings
         self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (216,21,36)
