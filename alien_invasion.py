bb79f4b6ec1b96fdb5a5331832f93cf470582841
NikFMatten <nfmatten@gmail.com>
2023-02-02 11:56:36 -0700

    feat: added prep_ships to check_play_button to reset number of lives/ships + in ship_hit method, if ship is hit and decremented lives, prep_ship is called to display correct number of lives remaining

--- alien_invasion.py
+++ alien_invasion.py
@@ -72,17 +72,22 @@ class AlienInvasion:
         if button_clicked and not self.stats.game_active:
             # Reset the game settings
             self.settings.initialize_dynamic_settings()
+
             # Reset the game statistics
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
+
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
             self.bullets.empty()
+
             # Create a new fleet and center the ship
             self._create_fleet()
             self.ship.center_ship()
+
             # Hide the mouse cursor
             pygame.mouse.set_visible(False)
 
@@ -159,8 +164,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien"""
         if self.stats.ships_left > 0:
-            # Decrement ships_left
+            # Decrement ships_left and update scoreboard
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             #get rid of any remaining aliens and bullets
             self.aliens.empty()
